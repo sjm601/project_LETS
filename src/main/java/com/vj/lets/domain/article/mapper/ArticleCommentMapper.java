c8b90f29dd39b8da404b822653b1d1ce3cfddf3b
gksqhfdl12 <gksqhfdl12@gmail.com>
2023-09-18 17:53:34 +0900

    aaaaa

--- src/main/java/com/vj/lets/domain/article/mapper/ArticleCommentMapper.java
+++ src/main/java/com/vj/lets/domain/article/mapper/ArticleCommentMapper.java
@@ -2,9 +2,6 @@ package com.vj.lets.domain.article.mapper;
 
 import com.vj.lets.domain.article.dto.ArticleComment;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
 
 /**
 * 클래스 설명 : ArticleCommentMapper 답글 매퍼 파일
@@ -15,11 +12,12 @@ import java.util.List;
 @Mapper
 public interface ArticleCommentMapper {
     /** 댓글 생성*/
-    public void create (ArticleComment articleComment, @Param("articleId")int id);
+    public void create (ArticleComment articleComment);
 
     /** 댓글 삭제*/
     public void delete (int id);
 
-    /** 댓글 목록*/
-   public List<ArticleComment> findCommentAll ();
+    /** id로 해당 댓글 찾기 (수정 ,삭제할 때 필요)*/
+    public ArticleComment findById(int id);
+
 }
--- src/main/resources/static/js/custom/mygroup.js
+++ src/main/resources/static/js/custom/mygroup.js
@@ -137,3 +137,5 @@ function contactMember(id, type) {
         });
     }
 }
+
+
