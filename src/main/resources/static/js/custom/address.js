f6d017d646588e4fc0b7db92465a5805f7e2d82f
heeyeong <gmldud5659@naver.com>
2023-09-20 10:12:13 +0900

    :bug: Fix create studyGroup sigunguName & sidoName

--- src/main/resources/static/js/custom/address.js
+++ src/main/resources/static/js/custom/address.js
@@ -1,10 +1,12 @@
 window.onload = function () {
-    const addressInputs = document.querySelectorAll('#address');
-    addressInputs.forEach((input, idx) => {
+    const sigunguInputs = document.querySelectorAll('.sigunguInputs');
+    const sidoInputs = document.querySelectorAll('.sidoInputs');
+    sigunguInputs.forEach((input, idx) => {
         input.addEventListener('click', () => {
             new daum.Postcode({
                 oncomplete: function (data) {
-                    addressInputs[idx].value = data.sigungu;
+                    sidoInputs[idx].value = data.sido;
+                    sigunguInputs[idx].value = data.sigungu;
                 }
             }).open();
         });
