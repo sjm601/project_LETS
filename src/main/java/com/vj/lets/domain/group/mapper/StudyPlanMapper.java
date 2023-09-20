dbb4af989a08200c238497f59b498ddd904a698d
Heeyeong <gmldud5659@naver.com>
2023-09-20 23:05:43 +0900

    :sparkles: Add studyPlan bean, mapper, xml

---
+++ src/main/java/com/vj/lets/domain/group/mapper/StudyPlanMapper.java
@@ -0,0 +1,50 @@
+package com.vj.lets.domain.group.mapper;
+
+import com.vj.lets.domain.group.dto.StudyPlan;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * 스터디 일정 매퍼
+ *
+ * @author VJ특공대 이희영
+ * @version 1.0
+ * @since 2023-09-20 (수)
+ */
+@Mapper
+public interface StudyPlanMapper {
+
+    /**
+     * 스터디 일정 생성
+     * 
+     * @author VJ특공대 이희영
+     * @param studyPlan 스터디 일정
+     */
+    public void createStudyPlan(StudyPlan studyPlan);
+
+    /**
+     * 스터디 그룹에 스터디 일정 리스트 조회
+     * 
+     * @author VJ특공대 이희영
+     * @param studyGroupId 스터디 그룹 아이디
+     * @return 스터디 일정 리스트
+     */
+    public List<StudyPlan> findAllStudyPlan(int studyGroupId);
+
+    /**
+     * 스터디 일정 수정
+     * 
+     * @author VJ특공대 이희영
+     * @param studyPlanId 스터디 일정 아이디
+     */
+    public void updateStudyPlan(int studyPlanId);
+
+    /**
+     *  스터디 일정 삭제
+     *  
+     * @author VJ특공대 이희영
+     * @param studyPlanId 스터디 일정 아이디
+     */
+    public void deleteStudyPlan(int studyPlanId);
+}
